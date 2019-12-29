db46c0dd594a8b0ebbf89959774cdff653afe4b8
Constantine Tarasenkov <constantine@protonmail.com>
2019-12-29 17:24:29 +0300

    Add a define in GLSL compiler command line

--- imgui.glsl
+++ imgui.glsl
@@ -1,16 +1,14 @@
 /*
-cgc -oglsl -strict -profile arbvp1 -entry VSMain imgui.glsl | sed -z 's/\n/\"\n\"/g'
-cgc -oglsl -strict -profile arbfp1 -entry PSMain imgui.glsl | sed -z 's/\n/\"\n\"/g'
+cgc -oglsl -strict -profile arbvp1 -entry VSMain -DVS imgui.glsl | sed -z 's/\n/\"\n\"/g'
+cgc -oglsl -strict -profile arbfp1 -entry PSMain      imgui.glsl | sed -z 's/\n/\"\n\"/g'
 */
-
-#define PS
 
 #version 130
 
 varying vec4 g_color;
 varying vec2 g_uv;
 
-#ifndef PS
+#ifdef VS
 void VSMain() {
   g_color     = gl_Color;
   g_uv        = gl_Vertex.zw;
